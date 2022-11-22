cbc4c1130d4fd4739452e0172a96c0c2a34b9294
hyeonjeong Park <py0429@ewhain.net>
2022-11-22 15:32:29 +0900

    cors error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
+import config from "./config";
 
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express(); // express 객체 받아옴
 const PORT = 3000; // 사용할 port를 3000번으로 설정
-const cors = require('cors');
+
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:3000", config.BaseUrl]
+}));
 
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 app.use("/", router); // use -> 모든 요청

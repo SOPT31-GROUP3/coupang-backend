da5a62e11c7d8de704619238f2886a705f8a1b65
hyeonjeong Park <py0429@ewhain.net>
2022-11-22 14:30:14 +0900

    [FIX] cors 에러

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
 
+
 const app = express(); // express 객체 받아옴
 const PORT = 3000; // 사용할 port를 3000번으로 설정
 const cors = require('cors');
 
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
-
 app.use("/", router); // use -> 모든 요청
-
-app.use( // cors 처리
-    cors({
-        credentials: true,
-        origin: "http://localhost:3000",
-    })
-);
 
 //* HTTP method - GET
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
